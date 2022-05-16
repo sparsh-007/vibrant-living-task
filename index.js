8316390be60352d8354f9ebac7747c0c8a924565
Sparsh Kapoor <93381005+sparsh-007@users.noreply.github.com>
2022-05-17 01:08:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onscroll = function () {
   var top = window.scrollY;
   console.log(top);
-  if (top>=875 && top<=923git) {
+  if (top>=875 && top<=923) {
     document.getElementById("cent").classList.add("active");
   } else{
     document.getElementById("cent").classList.remove("active");
